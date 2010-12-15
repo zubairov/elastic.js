4482e0a2c84ead483daeebdbb62cff07e1b9d8ac
Renat Zubairov <renat.zubairov@gmail.com>
2010-12-15 23:09:23 +0100

    Added tests. They are not working yet, fixing.

---
+++ test/allJobs.js
@@ -0,0 +1,62 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    Engine = require('../lib/elastic').Engine,
+    fs = require('fs'),
+    path = require('path');
+
+// Common vow to check that flow
+// execution is completed without errors
+var noError = function(err) {
+	if (err) {
+		fail(err);
+	}
+}
+
+// Function that takes context name and seraches a JSON file
+// with given name. Then validates it
+var validateJSON = function() {
+	var context = {
+        topic: function () {
+			var file = this.context.name;
+			var resourceName = path.join('lib/elastic/flows/' + file);
+			fs.readFile(resourceName, 'utf-8', this.callback);
+        }
+    };
+	context['should be a valid JSON'] = function(err, data) {
+		assert.isNull(err);
+		JSON.parse(data);
+	};
+    return context;
+}
+
+var listFiles = function(files) {
+	var result = {};
+	files.forEach(function(file) {
+		result[file] = validateJSON();
+	});
+	return result;
+}
+
+fs.readdir('lib/elastic/flows/', function(err, files) {
+	if (err) throw err;
+	vows.describe('Tesing flows JSON validity').addBatch({
+		'Iterate over all files in ../lib/elastic/flows': listFiles(files)
+	}).export(module);
+});
+
+vows.describe('Testing engine jobs').addBatch({
+    'Instantiating engine': {
+        topic: new (Engine),
+        'should respond to start': function(engine) {
+        	assert.isFunction(engine.start);
+        },
+       	'starting flow1': {
+       		topic: function(engine) { return engine.start('flow1', this.callback); },
+       		'should produce no errors': noError
+       	},
+       	'starting twitter': {
+       		topic: function(engine) { return engine.start('twitter', this.callback); },
+       		'should produce no errors': noError
+       	}
+    }
+}).export(module);
---
+++ test/utils.js
@@ -0,0 +1,85 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    fetchJSON = require('../lib/elastic/util').fetchJSON;
+    util = require('../lib/elastic/util');
+
+// Test for fetchJSON    
+vows.describe('Testing fetchJSON').addBatch({
+    'Fetching available resource': {
+        topic: function() {
+        	fetchJSON("http://twitter.com/statuses/user_timeline/zubairov.json", this.callback);
+        },
+        'should be ok': function(err, data) {
+        	assert.isNull(err);
+        	assert.isTrue(data.length > 10);
+        }
+	},
+    'Fetching 404 resource': {
+        topic: function() {
+        	fetchJSON("http://www.google.com/nonExistingResource", this.callback);
+        },
+        'should be ok': function(err, data) {
+        	assert.isNotNull(err);
+        	assert.include (err, '404');
+        	assert.isUndefined(data);
+        }
+	},
+	'Fetching 301 resource': {
+        topic: function() {
+        	fetchJSON("http://google.com/nonExistingResource", this.callback);
+        },
+        'should be ok': function(err, data) {
+        	assert.isNotNull(err);
+        	assert.include (err, '404');
+        	assert.isUndefined(data);
+        }
+	}
+}).export(module);
+
+// Test for XML to JSON parsing
+vows.describe('Testing XML conversion to JSON').addBatch({
+	'Eeach nested element or attribute is represented as a name/value property of the object' : {
+		topic: function() {
+			var parser = util.parser();
+			parser.on('json', this.callback);
+			parser.write('<xml>Hello, <who name="world">world</who>!</xml>').close();
+		},
+		'expectations match': function(data, arg1) {
+			assert.deepEqual(data, { 'xml' : {
+					"$t":	"Hello, !",
+					'who' : { 'name': 'world', "$t": "world" }
+				}
+			});
+		}
+	},
+	'Namespaces are also supported': {
+		topic: function() {
+			var parser = util.parser();
+			parser.on('json', this.callback);
+			parser.write('<n1:test xmlns:n1="namespace1">content</n1:test>').close();
+		},
+		'namespaces validated ok': function(data, arg1) {
+			assert.deepEqual({ 'n1$test' : {
+					"$t":	"content",
+					'xmlns$n1' : "namespace1"
+				}
+			}, data);
+		}
+	},
+	'Repititive elements are transformed into arrays': {
+		topic: function() {
+			var parser = util.parser();
+			parser.on('json', this.callback);
+			parser.write('<root><msg>Hello</msg><msg>World</msg></root>').close();
+		},
+		'match expectations': function(data, arg1) {
+			assert.deepEqual(data, { 'root' : {
+					"msg": [
+						{'$t': 'Hello'},
+						{'$t': 'World'}
+					]
+				}
+			});
+		}
+	}
+}).export(module);
